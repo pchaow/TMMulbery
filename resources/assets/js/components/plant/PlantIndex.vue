0b682d684221363a2a811d6409633a15a41efeb9
Dede <chontipan@gmail.com>
2017-01-28 17:38:35 +0700

    plantIndex

--- resources/assets/js/components/plant/PlantIndex.vue
+++ resources/assets/js/components/plant/PlantIndex.vue
@@ -12,10 +12,10 @@
                 <div class="panel-body">
 
                     <div class="col-lg-12">
-                        <form class="form-horizontal">
+                        <form class="form-horizontal" v-on:submit.prevent="search">
                             <div class="form-group">
                                 <label>คำสำคัญ : </label>
-                                <input class="form-control"
+                                <input class="form-control" v-model="form.keyword" @keypress
                                        placeholder="กรอกคำสำคัญที่ต้องการค้นหา"/>
                             </div>
 
@@ -33,7 +33,7 @@
             <div class="panel panel-success">
                 <div class="panel-heading">
                     <div class="btn-group btn-group-sm pull-right">
-                        <a class="btn btn-success" style="padding-bottom: 2px"><i
+                        <a v-bind:href="createUrl" class="btn btn-success" style="padding-bottom: 2px"><i
                                 class="fa fa-plus"></i> เพิ่มแปลงหม่อน</a>
                     </div>
                     <i class="fa fa-tree"></i>
@@ -57,7 +57,7 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <tr>
+                                <tr v-for="p in plants">
                                     <td>กอไก่ ใจดี</td>
                                     <td>A1</td>
                                     <td>100,000</td>
@@ -73,6 +73,7 @@
                                     </td>
 
                                 </tr>
+<<<<<<< HEAD
                                 <tr>
                                     <td>กอไก่ ใจดี</td>
                                     <td>A2</td>
@@ -124,16 +125,20 @@
                                     </td>
                                 </tr>
 
+=======
+>>>>>>> origin/master
                                 </tbody>
                                 <tfoot>
                                 <tr>
-                                    <td colspan="5">
+                                    <td colspan="4">
                                         <div>
-                                            จำนวนทั้งหมด 4 รายการ
+                                            จำนวนทั้งหมด {{page.total}} รายการ
                                         </div>
                                         <ul class="pagination">
-                                            <li>
+                                            <li v-bind:class="{ 'active' : (page.current_page == n) }"
 
+                                                v-for="n in page.last_page ">
+                                                <a style="cursor: default;" v-on:click="gotoPage(n)">{{ n }}</a>
                                             </li>
                                         </ul>
                                     </td>
@@ -151,5 +156,67 @@
 </template>
 
 <script>
+
+    export default {
+        props: {
+            createUrl : String,
+            loadUrl : String,
+            deleteUrl : String,
+        },
+        data() {
+            return {
+                roles: [],
+                page: {},
+                form: {
+                    keyword: "",
+                    page: 1,
+                }
+            }
+        },
+        methods: {
+            strFormat : window.strFormat,
+            reset: function () {
+                this.form = {
+                    keyword: "",
+                    page: 1,
+                }
+            },
+            search: function () {
+                this.form.page = 1
+                this.load();
+            },
+            gotoPage: function (n) {
+                this.form.page = n;
+                this.load()
+            },
+            load: function () {
+                this.$http.get(this.loadUrl, {
+                    params: this.form
+                }).then(function (r) {
+//                    console.log(r.data)
+                    this.page = r.data
+                    this.plants = this.page.data
+                })
+            },
+            deletePlant: function (plant) {
+                if (confirm("คุณต้องการลบแปลงนี้หรือไม่ ?")) {
+                    this.$http.delete(this.strFormat(this.deleteUrl,{id:plant.id}), {
+                        params: this.form
+                    }).then(function (r) {
+                        this.load()
+                    })
+                }
+
+            }
+        },
+        mounted() {
+            console.log(this.loadUrl);
+            this.load();
+        }
+    }
+
+
+
+
 
 </script>

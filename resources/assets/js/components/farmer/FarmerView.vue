d9611ece8e29bc5cbf8c7e3b27a0205ce17f5d10
Dede <chontipan@gmail.com>
2017-01-30 19:37:42 +0700

    edit create,edir farmer and add view-farmer

--- resources/assets/js/components/farmer/FarmerView.vue
+++ resources/assets/js/components/farmer/FarmerView.vue
@@ -1,150 +1,91 @@
 <template>
-    <div class="row">
-        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-3">
 
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    Edit User
-                </div>
+                <!-- Profile Image -->
+                <div class="box box-primary">
+                    <div class="box-body box-profile">
+                        <h3 class="profile-username text-center">กอไก่ ใจดี</h3>
 
-                <div class="panel-body">
+                        <ul class="list-group list-group-unbordered">
+                            <li class="list-group-item">
+                                <b>แปลงทั้งหมด</b> <a class="pull-right">7</a>
+                            </li>
 
-                    <div class="col-lg-12">
-                        <form class="form-horizontal" v-on:submit.prevent="save">
+                            <li class="list-group-item">
+                                <b>ซื้อขาย</b> <a class="pull-right">287</a>
+                            </li>
+                        </ul>
 
-                            <fieldset>
-
-                                <div class="form-group">
-                                    <h4>ข้อมูลส่วนตัว</h4>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['identity'] }">
-                                    <label class="control-label">รหัสประจำตัว : </label>
-                                    <input class="form-control" placeholder="Identity ID"
-                                           v-model="formInputs.identity"/>
-                                    <span v-if="formErrors['identity']"
-                                          class="help-block">{{ formErrors['identity'] }}</span>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['name'] }">
-                                    <label class="control-label">ชื่อ-นามสกุล : </label>
-                                    <input class="form-control" placeholder="Name" v-model="formInputs.name"/>
-                                    <span v-if="formErrors['name']"
-                                          class="help-block">{{ formErrors['name'] }}</span>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['contact_number'] }">
-                                    <label class="control-label">เบอร์โทรศัพท์ : </label>
-                                    <input class="form-control" placeholder="เบอร์โทรศัพท์"
-                                           v-model="formInputs.contact_number"/>
-                                    <span v-if="formErrors['contact_number']" class="help-block">{{ formErrors['contact_number'] }}</span>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['address'] }">
-                                    <label class="control-label">ที่อยู่ : </label>
-                                    <input class="form-control" placeholder="ที่อยู่" v-model="formInputs.address"/>
-                                    <span v-if="formErrors['address']"
-                                          class="help-block">{{ formErrors['address'] }}</span>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['province_id'] }">
-                                    <label class="control-label">จังหวัด : </label>
-                                    <select class="form-control" v-model="formInputs.province_id">
-                                        <option>กรุณาเลือก</option>
-                                        <option>AAA</option>
-                                        <option>AAA</option>
-                                    </select>
-                                    <span v-if="formErrors['province_id']" class="help-block">{{ formErrors['province_id'] }}</span>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['amphure_id'] }">
-                                    <label class="control-label">อำเภอ : </label>
-                                    <select class="form-control" v-model="formInputs.amphure_id">
-                                        <option>กรุณาเลือก</option>
-                                        <option>AAA</option>
-                                        <option>AAA</option>
-                                    </select>
-                                    <span v-if="formErrors['amphure_id']"
-                                          class="help-block">{{ formErrors['amphure_id'] }}</span>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['district_id'] }">
-                                    <label class="control-label">ตำบล : </label>
-                                    <select class="form-control" v-model="formInputs.district_id">
-                                        <option>กรุณาเลือก</option>
-                                        <option>AAA</option>
-                                        <option>AAA</option>
-                                    </select>
-                                    <span v-if="formErrors['district_id']" class="help-block">{{ formErrors['district_id'] }}</span>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['postal_code'] }">
-                                    <label class="control-label">รหัสไปรษณีย์ : </label>
-                                    <input class="form-control" placeholder="รหัสไปรษณีย์"
-                                           v-model="formInputs.postal_code"/>
-                                    <span v-if="formErrors['postal_code']" class="help-block">{{ formErrors['postal_code'] }}</span>
-                                </div>
-                            </fieldset>
-
-                            <fieldset>
-
-                                <div class="form-group">
-                                    <h4>ข้อมูลการเข้าใช้งานระบบ</h4>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['email'] }">
-                                    <label class="control-label">E-mail : </label>
-                                    <input class="form-control" placeholder="E-mail" v-model="formInputs.email"/>
-                                    <span v-if="formErrors['email']" class="help-block">{{ formErrors['email'] }}</span>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['username'] }">
-                                    <label class="control-label">รหัสผู้ใช้ : </label>
-                                    <input type="text" class="form-control" placeholder="Username"
-                                           v-model="formInputs.username"/>
-                                    <span v-if="formErrors['username']"
-                                          class="help-block">{{ formErrors['username'] }}</span>
-                                </div>
-
-                                <div class="form-group" v-bind:class="{ 'has-error': formErrors['password'] }">
-                                    <label class="control-label">รหัสผ่าน : </label>
-                                    <input type="password" class="form-control" placeholder="Password"
-                                           v-model="formInputs.password"/>
-                                    <span v-if="formErrors['password']"
-                                          class="help-block">{{ formErrors['password'] }}</span>
-                                </div>
-
-                                <div class="form-group"
-                                     v-bind:class="{ 'has-error': formErrors['password_confirmation '] }">
-                                    <label class="control-label">ยืนยันรหัสผ่าน : </label>
-                                    <input type="password" class="form-control" placeholder="Password Confirmation"
-                                           v-model="formInputs.password_confirmation "/>
-                                    <span v-if="formErrors['password_confirmation ']" class="help-block">{{ formErrors['password_confirmation '] }}</span>
-                                </div>
-                            </fieldset>
-
-
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                                <a v-bind:href="successUrl" class="btn btn-default">Cancel</a>
-                            </div>
-                        </form>
+                        <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
                     </div>
+                    <!-- /.box-body -->
                 </div>
+                <!-- /.box -->
 
+                <!-- About Me Box -->
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">About Me</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <strong><i class="fa fa-book margin-r-5"></i> Education</strong>
+
+                        <p class="text-muted">
+                            B.S. in Computer Science from the University of Tennessee at Knoxville
+                        </p>
+
+                        <hr>
+
+                        <strong><i class="fa fa-map-marker margin-r-5"></i> Location</strong>
+
+                        <p class="text-muted">Malibu, California</p>
+
+                        <hr>
+
+                        <strong><i class="fa fa-pencil margin-r-5"></i> Skills</strong>
+
+                        <p>
+                            <span class="label label-danger">UI Design</span>
+                            <span class="label label-success">Coding</span>
+                            <span class="label label-info">Javascript</span>
+                            <span class="label label-warning">PHP</span>
+                            <span class="label label-primary">Node.js</span>
+                        </p>
+
+                        <hr>
+
+                        <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
             </div>
+            <!-- /.col -->
+
+            <!-- /.col -->
         </div>
-    </div>
+        <!-- /.row -->
+
+
+
+
+
+
+
 </template>
 
 <script>
     export default {
         props: {
             userId: Number,
-            saveUrl : String,
-            loadUrl : String,
-            successUrl : String,
-            loadRolesUrl : String,
+            saveUrl: String,
+            loadUrl: String,
+            successUrl: String,
+            loadRolesUrl: String,
         },
         data() {
             return {
@@ -157,30 +98,30 @@
             save: function () {
                 this.formErrors = [];
                 this.$http.put(this.saveUrl, this.formInputs)
-                        .then((response) => {
-                            // success callback
-                            // console.log(response);
-                            window.location.href = this.successUrl
-                        }, (response) => {
-                            // error callback
-                            this.formErrors = response.data;
-                        });
+                    .then((response) => {
+                        // success callback
+                        // console.log(response);
+                        window.location.href = this.successUrl
+                    }, (response) => {
+                        // error callback
+                        this.formErrors = response.data;
+                    });
 
 
             },
             load: function () {
 
                 this.$http.get(this.loadUrl)
-                        .then((response) => {
-                            // success callback
-                            // console.log(response);
-                            this.formInputs = response.data;
-                            this.reInitialMultiOption(this.roles,this.formInputs.roles);
+                    .then((response) => {
+                        // success callback
+                        // console.log(response);
+                        this.formInputs = response.data;
+                        this.reInitialMultiOption(this.roles, this.formInputs.roles);
 
-                        }, (response) => {
-                            // error callback
-                            this.formErrors = response.data;
-                        })
+                    }, (response) => {
+                        // error callback
+                        this.formErrors = response.data;
+                    })
             },
             loadRoles: function () {
                 return this.$http.get(this.loadRolesUrl, {
@@ -192,7 +133,7 @@
 
                 })
             },
-            reInitialMultiOption: function (master,selected) {
+            reInitialMultiOption: function (master, selected) {
 
                 var choiceOpt = master
                 var userOpt = selected
@@ -219,19 +160,6 @@
             this.load()
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 </script>

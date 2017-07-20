0e5c2c9ad8301625bf429d1d5a1c6b0ba3ad50e0
Dede <chontipan@gmail.com>
2017-07-20 22:22:32 +0700

    edit label home

--- resources/views/buyer/home.blade.php
+++ resources/views/buyer/home.blade.php
@@ -4,7 +4,6 @@
     <section class="content-header">
         <h1>
             Dashboard
-            <small>Optional description</small>
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
--- resources/views/farmer/home.blade.php
+++ resources/views/farmer/home.blade.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <h1>
             Dashboard
-            <small>Optional description</small>
+
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>

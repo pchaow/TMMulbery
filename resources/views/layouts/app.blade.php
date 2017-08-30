a3141fe3951c364d50ba931942c0436caed23510
Dede <chontipan@gmail.com>
2017-08-30 21:44:15 +0700

    edit label Piankusol

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@ $avatarUrl = "/dist/img/basicAvatar.jpg"
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>P</b></span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Piansukol </b>Mumberry Harvesting</span>
+            <span class="logo-lg"><b>Piankusol </b>Mumberry Harvesting</span>
         </a>
 
         <!-- Header Navbar -->
